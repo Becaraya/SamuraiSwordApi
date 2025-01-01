559e9059376de068a5ab8d81d30f7d1d4981886e
becaraya <becaraya@student.42.fr>
2025-01-01 14:05:31 +0100

    Initial commit

---
+++ script.js
@@ -0,0 +1,8 @@
+// script.js
+document.getElementById('startBtn').addEventListener('click', function() {
+    // Logique du jeu
+});
+
+document.getElementById('rulesBtn').addEventListener('click', function() {
+    // Télécharger les règles
+});
